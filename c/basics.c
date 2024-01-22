566808834555ac73fd5f557a69f225f70f4bfcfd
brightburst641 <2027relova.kenzo@fcboe.org>
2024-01-22 14:47:48 -0500

    Edited portfolio

---
+++ c/basics.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+
+	printf("This is my c basics file\n");
+
+	int num;
+	printf("What is your number? ");
+	scanf("%i", &num);
+	printf("Your number is %i\n", num);
+
+	return 0;
+}
